bab11a54ee502b4078b13a91642e35c7b35c0225
Andrei Humulescu <Andrei.Humulescu@endava.com>
2021-11-25 11:09:01 +0200

    TR-174: [Integrations]: Reverted the set-payment-method file, in case there are merchants that want to use it

--- RealexPayments/HPP/view/frontend/web/js/action/set-payment-method.js
+++ RealexPayments/HPP/view/frontend/web/js/action/set-payment-method.js
@@ -1,10 +1,16 @@
 define(
     [
         'jquery',
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/model/quote',
+        'Magento_Checkout/js/model/url-builder',
+        'mage/storage',
+        'Magento_Customer/js/model/customer',
+        'Magento_Checkout/js/model/full-screen-loader'
     ],
-    function($, Component) {
+    function($, quote, urlBuilder, storage, customer, fullScreenLoader) {
         'use strict';
+        var agreementsConfig = window.checkoutConfig.checkoutAgreements;
+
         /**
          * This function adds extension attributes to paymentData
          * the attributes param must be an array of objects,
@@ -24,30 +30,65 @@ define(
             }
         }
 
-        return function (extensionAttributes) {
-            return Component.extend({
-                defaults: {
-                    redirectAfterPlaceOrder: false,
-                    pluginData: {
-                        'method': this.getCode()
-                    }
-                },
+        return function(extensionAttributes) {
 
-                getData: function() {
-                    /**
-                     * If we have the extensionAttributes parameter,
-                     * we add them to the paymentData object
-                     */
-                    if (extensionAttributes && extensionAttributes.length > 0) {
-                        if (!this.pluginData.extension_attributes) {
-                            this.pluginData.extension_attributes = {};
-                        }
-                        addExtensionAttributes(extensionAttributes, this.pluginData);
-                    }
+            var serviceUrl,
+                payload,
+                paymentData = quote.paymentMethod();
+            if (paymentData.title) {
+                delete paymentData.title;
+            }
+            // check if agreement is enabled if so add it to payload
+            if (agreementsConfig.isEnabled) {
+                var agreementForm = $('.payment-method._active div[data-role=checkout-agreements] input'),
+                    agreementData = agreementForm.serializeArray(),
+                    agreementIds = [];
 
-                    return this.pluginData;
+                agreementData.forEach(function(item) {
+                    agreementIds.push(item.value);
+                });
+
+                paymentData.extension_attributes = {
+                    agreement_ids: agreementIds
+                };
+            }
+
+            /**
+             * If we have the extensionAttributes parameter,
+             * we add them to the paymentData object
+             */
+            if (extensionAttributes && extensionAttributes.length > 0) {
+                if (!paymentData.extension_attributes) {
+                    paymentData.extension_attributes = {};
                 }
-            });
-        }
+                addExtensionAttributes(extensionAttributes, paymentData);
+            }
+
+            /** Checkout for guest and registered customer. */
+            if (!customer.isLoggedIn()) {
+                serviceUrl = urlBuilder.createUrl('/guest-carts/:quoteId/payment-information', {
+                    quoteId: quote.getQuoteId()
+                });
+                payload = {
+                    cartId: quote.getQuoteId(),
+                    email: quote.guestEmail,
+                    paymentMethod: paymentData,
+                    billingAddress: quote.billingAddress()
+                };
+            } else {
+                serviceUrl = urlBuilder.createUrl('/carts/mine/payment-information', {});
+                payload = {
+                    cartId: quote.getQuoteId(),
+                    paymentMethod: paymentData,
+                    billingAddress: quote.billingAddress()
+                };
+            }
+            if (Object.prototype.hasOwnProperty.call(paymentData, '__disableTmpl')) { delete paymentData.__disableTmpl; }
+            fullScreenLoader.startLoader();
+
+            return storage.post(
+                serviceUrl, JSON.stringify(payload)
+            );
+        };
     }
 );

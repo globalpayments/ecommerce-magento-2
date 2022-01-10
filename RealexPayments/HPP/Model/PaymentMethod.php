4099c5ecc9abbf9338d71fde41c778aacc64cc44
Andrei Humulescu <Andrei.Humulescu@endava.com>
2022-01-10 11:48:45 +0200

    TR-186: [Integrations]: Fixed a bug where you could not partially refund an order that was created with a Gift Card

--- RealexPayments/HPP/Model/PaymentMethod.php
+++ RealexPayments/HPP/Model/PaymentMethod.php
@@ -1356,6 +1356,15 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod impleme
     public function refund(\Magento\Payment\Model\InfoInterface $payment, $amount)
     {
         parent::refund($payment, $amount);
+
+        /**
+         * If the amount is 0, it means that the user is refunding a Gift Card,
+         * so we do not send it to HPP
+         */
+        if ((float) $amount === 0.00) {
+            return $this;
+        }
+
         $order = $payment->getOrder();
         $comments = $payment->getCreditMemo()->getComments();
         $grandTotal = $order->getBaseGrandTotal();
